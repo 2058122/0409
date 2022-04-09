a645cfd489410a643a470ddfe413813a3fc29a90
2058122 <xinglhsk@ezweb.ne.jp>
2022-04-09 17:39:42 +0900

    0409

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const request = require("request");
+const request = require("request");
 const app = express();
 const PORT = process.env.PORT || 3000; //Heroku用
 
@@ -12,4 +12,19 @@ app.post("/", function (req, res) {
   console.log("req.body", data);
   res.send("api: ok");
 });
+
+const options = {
+  url: `https://api-data.line.me/v2/bot/message/${req.body.events[0].message.id}/content`,
+  method: "get",
+  headers: {
+    Authorization: "Bearer " + accessToken,
+  },
+  encoding: null,
+};
+
+request(options, function (error, response, body) {
+  const buffer = new Buffer.from(body);
+  console.log(buffer);
+});
+
 process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用
