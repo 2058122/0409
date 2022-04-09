25c012171267961121c4e48e3e0aa097291ae292
2058122 <xinglhsk@ezweb.ne.jp>
2022-04-09 16:39:23 +0900

    0409

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const request = require("request");
+//const request = require("request");
 const app = express();
 const PORT = process.env.PORT || 3000; //Heroku用
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
 
 app.post("/", function (req, res) {
   //console.log(req.body);
-  //const data = req.body;
-  //console.log("req.body", data);
-  //res.send("api: ok");
+  const data = req.body;
+  console.log("req.body", data);
+  res.send("api: ok");
 });
 process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用
