bb856574c2ba996a447e21d9bc9efd7d309ba6c0
2058122 <xinglhsk@ezweb.ne.jp>
2022-04-09 16:37:36 +0900

    0409

--- api/index.js
+++ api/index.js
@@ -1,40 +1,15 @@
-/*
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-
-process.env.NOW_REGION ? (module.exports = app) : app.listen(PORT);
-*/
-
 const express = require("express");
 const request = require("request");
 const app = express();
 const PORT = process.env.PORT || 3000; //Heroku用
-const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post("/", function (req, res) {
-  console.log(req.body);
-  const data = req.body;
-  console.log("req.body", data);
-  res.send("api: ok");
-});
-app.get("/api/get/", (req, res) => {
-  res.json({ message: "こちらはルートパスです" });
-});
-app.use(bodyParser.json());
-app.post("/api/post", (req, res) => {
-  console.log(req.body);
-  res.send("POSTされたデータを取得できました");
-});
-//ローカル用サーバ
-app.listen(3000, () => {
-  console.log("Application started");
+  //console.log(req.body);
+  //const data = req.body;
+  //console.log("req.body", data);
+  //res.send("api: ok");
 });
 process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用
