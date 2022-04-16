19a0c3e6224749e4ad65c8fee881521abec17fe8
2058122 <xinglhsk@ezweb.ne.jp>
2022-04-16 16:24:59 +0900

    0416

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-//const request = require("request");
+const request = require("request");
 const app = express();
+const accessToken =
+  "mT1PIkPaE29mNovFkm0MhTeRF+5V9bXK0EU8iwMgS4K1OBrry+NW6FQpY/67tNTDG339pv0rVRaESIlIJPO4XrbqoliIZaZQkLdgDtJlr1eZqEg/Mh13NtCgPyfF4vOE/OS7yaT/63VLSCpPZtEFqgdB04t89/1O/w1cDnyilFU=";
 const PORT = process.env.PORT || 3000; //Heroku用
 
 app.use(express.json());
@@ -9,7 +11,62 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/", function (req, res) {
   //console.log(req.body);
   const data = req.body["events"][0]["message"]["id"];
+  const replyToken = req.body["events"][0]["replyToken"];
+
   console.log("req.body", data);
   res.send("api: ok");
+  const options = {
+    url: `https://api-data.line.me/v2/bot/message/data/content`,
+    method: "get",
+    headers: {
+      Authorization: "Bearer " + accessToken,
+    },
+    encoding: null,
+  };
+
+  request(options, function (error, response, body) {
+    const buffer = new Buffer.from(body);
+    console.log(buffer);
+    const option = {
+      uri: "https://leadhacktesteastjapan-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/3995706a-5622-438d-afce-71de27a57ca5/classify/iterations/Iteration1/image",
+      method: "post",
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "Prediction-Key": "034d56b2970f4167b38c1278b84ecdb7",
+      },
+      body: buffer,
+    };
+
+    request.post(option, function (error, res, body) {
+      console.log(body);
+      console.log(replyToken);
+      const resBody = JSON.parse(body);
+
+      const messageData = {
+        replyToken: replyToken,
+        messages: [
+          {
+            type: "text",
+            text: "res.OK",
+          },
+        ],
+      };
+
+      const optionsLine = {
+        uri: "https://api.line.me/v2/bot/message/reply",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + accessToken,
+        },
+        json: messageData,
+      };
+
+      // const optionLINE = JSON.stringify(optionsLine);
+      request.post(optionsLine, function (error, res, body) {
+        console.log("test OK");
+      });
+    });
+  });
 });
+
 process.env.NOW_REGION ? (module.express = app) : app.listen(PORT); //Heroku用
